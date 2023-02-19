9aad88f48cfc49745de788a606c8f395618a0f79
VascoMiguens <v_miguens@hotmail.com>
2023-02-19 13:09:53 +0000

    - register route for asset cachicng - workbox plugins for service worker and manifest file - css loader and babel webpack

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,30 +1,46 @@
-const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
-const { CacheFirst } = require('workbox-strategies');
-const { registerRoute } = require('workbox-routing');
-const { CacheableResponsePlugin } = require('workbox-cacheable-response');
-const { ExpirationPlugin } = require('workbox-expiration');
-const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
-
+const { warmStrategyCache } = require("workbox-recipes");
+const { CacheFirst } = require("workbox-strategies");
+const { registerRoute } = require("workbox-routing");
+const { CacheableResponsePlugin } = require("workbox-cacheable-response");
+const { ExpirationPlugin } = require("workbox-expiration");
+const { precacheAndRoute } = require("workbox-precaching/precacheAndRoute");
+const { StaleWhileRevalidate } = require("workbox-strategies");
 precacheAndRoute(self.__WB_MANIFEST);
 
 const pageCache = new CacheFirst({
-  cacheName: 'page-cache',
+  cacheName: "page-cache",
   plugins: [
-    new CacheableResponsePlugin({
+    // cache responses from a server, in this case 0 and 200.
+new CacheableResponsePlugin({
       statuses: [0, 200],
     }),
-    new ExpirationPlugin({
+   // set the expiration time to 30 days for the cached files
+ new ExpirationPlugin({
       maxAgeSeconds: 30 * 24 * 60 * 60,
     }),
   ],
 });
 
+// The warmStrategyCache function will use the URLs and caching strategy to pre-cache the specified pages.
 warmStrategyCache({
-  urls: ['/index.html', '/'],
+  urls: ["/index.html", "/"],
   strategy: pageCache,
 });
 
-registerRoute(({ request }) => request.mode === 'navigate', pageCache);
+//register route with a condition that checks if the request mode is "navigate", if it is use the pageCache.
+registerRoute(({ request }) => request.mode === "navigate", pageCache);
 
-// TODO: Implement asset caching
-registerRoute();
+registerRoute(
+  // define the callback function that will filter the requests we want to cache (in this case, JS and CSS files)
+({ request }) => ["style", "script", "worker"].includes(request.destination),
+  new StaleWhileRevalidate({
+    // Name of the cache storage.
+    cacheName: "asset-cache",
+    plugins: [
+      // This plugin will cache responses with these headers to a maximum-age of 30 days
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+    ],
+  })
+);
